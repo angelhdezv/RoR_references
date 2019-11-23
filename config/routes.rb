d5730efd749bd563cd9dca03e1638c7c8ff6f9a9
angelhdezv <angelhdezv@users.noreply.github.com>
2019-11-23 15:49:18 -0600

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
   resources :tratamientos
   resources :medicamentos
   resources :receta
   resources :mascota
   resources :doctors
   resources :duenios
+ 
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
